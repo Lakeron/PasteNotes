d1362541f44acb4ea7a9991a5a1d2fa52c0d9f31
Andrej Baran <lakeron@gmail.com>
2013-05-30 19:29:02 +0200

    update

---
+++ app/model/BaseModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lakeron
+ * Date: 3/30/13
+ * Time: 11:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Model;
+
+
+class BaseModel {
+
+}
---
+++ www/js/jquery.livequery.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: lakeron
+ * Date: 3/31/13
+ * Time: 9:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
